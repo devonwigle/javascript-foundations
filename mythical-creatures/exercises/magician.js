3828fde4c772654a7678d497444ea34ee04d56a9
Devon Wigle <devonwigle@gmail.com>
2021-10-20 16:52:11 -0500

    Complete prefer wearing top hat test

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -2,6 +2,7 @@ class Magician {
   constructor(name) {
     this.name = `The Great ${name.name}`;
     this.assistant = name.assistant
+    this.favoriteAccessory = 'top hat'
   }
 }
 module.exports = Magician
