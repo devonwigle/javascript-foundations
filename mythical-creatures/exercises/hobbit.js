22b267d258637adad190b58ff8618061ca9f354b
Devon Wigle <devonwigle@gmail.com>
2021-10-20 09:14:21 -0500

    Complete old age test

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -5,12 +5,17 @@ class Hobbit {
     }
     this.name = name.name;
     this.age = 0;
-    this.adult = false
+    this.adult = false;
+    this.old = false;
   }
   celebrateBirthday() {
     this.age ++
-    if (this.age >= 33) {
+    if (this.age >= 33 && this.age < 101) {
       this.adult = true
+    } else {
+      if (this.age >= 101) {
+        this.old = true
+      }
     }
   }
 
