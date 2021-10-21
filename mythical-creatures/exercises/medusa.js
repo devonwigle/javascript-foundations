605672c8a1dd8d1698f842a859fc30732ca648a6
Devon Wigle <devonwigle@gmail.com>
2021-10-21 08:11:45 -0500

    Complete freed statue to person

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -10,7 +10,9 @@ class Medusa {
     var newStatue = new Statue(victim.name)
     this.statues.push(newStatue)
     if (this.statues.length > 3) {
-      this.statues.shift()
+      var freePerson = new Person(this.statues[0].name)
+      this.statues.shift(freePerson)
+      return freePerson
     }
   }
 }
