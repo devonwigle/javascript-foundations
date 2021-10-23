d36a62b2673d0c3b2269afa191a4af24e11a2ef3
Devon Wigle <devonwigle@gmail.com>
2021-10-23 09:44:43 -0500

    Pass shoot, run, shoot, cranky test

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -4,11 +4,20 @@ class Centaur {
     this.breed = name.type;
     this.cranky = false;
     this.standing = true;
+    this.activeCount = 0;
   }
   shootBow() {
+    this.activeCount ++
+    if (this.activeCount >= 3) {
+      this.cranky = true
+    }
     return 'Twang!!!'
   }
   run() {
+    this.activeCount ++
+    if (this.activeCount >= 3) {
+      this.cranky = true
+    }
     return 'Clop clop clop clop!!!'
   }
 
