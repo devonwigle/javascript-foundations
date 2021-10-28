3f645b28f95b1be27b78de89b0487deacb02de7f
Devon Wigle <devonwigle@gmail.com>
2021-10-28 09:35:14 -0500

    Pass hunt white walkers when not protecting starks test

--- mythical-creatures/exercises/direwolf.js
+++ mythical-creatures/exercises/direwolf.js
@@ -6,11 +6,13 @@ class Direwolf {
     this.home = home || 'Beyond the Wall'
     this.size = size || 'Massive'
     this.starksToProtect = []
+    this.huntsWhiteWalkers = true
   }
   protect(stark) {
     if (this.home === stark.location && this.starksToProtect.length < 2) {
-     this.starksToProtect.push(stark)
-     stark.safe = true
+      this.starksToProtect.push(stark)
+      stark.safe = true
+      this.huntsWhiteWalkers = false
     }
   }
 }
