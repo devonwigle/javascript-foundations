f63f2844338a5386a093c44195a14420541667ce
Devon Wigle <devonwigle@gmail.com>
2021-11-04 06:07:45 -0500

    Pass human only notice every third test

--- mythical-creatures/exercises/human.js
+++ mythical-creatures/exercises/human.js
@@ -1,7 +1,14 @@
 class Human {
   constructor(person) {
-    this.name = person
-    this.encounterCounter = 0
+    this.name = person;
+    this.encounterCounter = 0;
+    this.noticeOgre = false;
+  }
+  noticesOgre() {
+    if ((this.encounterCounter % 3) != 0) {
+      return this.noticeOgre = false
+    }
+    return this.noticeOgre = true
   }
 }
 
