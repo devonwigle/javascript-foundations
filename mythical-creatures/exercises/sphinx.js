539f9f6408a55849d2e9ee283cce0ece657c34c1
Devon Wigle <devonwigle@gmail.com>
2021-11-06 08:49:16 -0500

    Pass only collect three riddles test

--- mythical-creatures/exercises/sphinx.js
+++ mythical-creatures/exercises/sphinx.js
@@ -4,7 +4,12 @@ class Sphinx {
     this.riddles = [];
   }
   collectRiddle(riddle) {
-    this.riddles.push(riddle)
+    if (this.riddles.length < 3) {
+      this.riddles.push(riddle)
+    } else {
+      this.riddles.shift()
+      this.riddles.push(riddle)
+    }
   }
 }
 
